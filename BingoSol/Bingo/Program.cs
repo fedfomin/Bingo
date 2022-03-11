0a3e16924980ecd336620ada47bfc71eceb90c08
fedir.fomin <fedir.fomin@WINAPRY5UDBQB8H>
2022-03-11 23:14:21 +0100

    Sol

--- BingoSol/Bingo/Program.cs
+++ BingoSol/Bingo/Program.cs
@@ -6,7 +6,7 @@ namespace Bingo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("** Welcome to Bingo app **");
         }
     }
 }
